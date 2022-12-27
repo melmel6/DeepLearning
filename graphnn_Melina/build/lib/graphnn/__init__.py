1af4fdcb0143dcbed527503fff66070b02a86614
melmel6 <melina.siskou@gmail.com>
2022-12-27 19:49:59 +0200

    cleanup

--- graphnn_Melina/build/lib/graphnn/__init__.py
+++
@@ -1,6 +0,0 @@
-from graphnn import calculator
-from graphnn import data
-from graphnn import layer
-from graphnn import model
-from graphnn import model_forces
-from graphnn import model_uncertainty
